ea5937a1958da895bda6e16780f28c22142a3cb4
Karol Groszewski <karol.groszewski@outlook.com>
2019-03-14 15:36:07 +0000

    Implemented data display.

---
+++ simulatedDataJS.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', displayData, false);
+
+function displayData() {
+    var JSONString = localStorage.getItem('StoredData');
+    var data = JSON.parse(JSONString);
+    console.table(data);
+
+    var html = "<tr>";
+    html += "<th>Index</th>";
+    html += "<th>First Name</th>";
+    html += "<th>Second Name</th>";
+    html += "<th>Gender</th>";
+    html += "<th>Age</th>";
+    html += "<th>Postcode</th>";
+    html += "<th>CHI Number</th>";
+    html += "<th>BMI</th>";
+    html += "</tr>";
+
+    for (var i = 0; i < data.length; i++) {
+        html += "<tr>";
+        html += "<td>" + i + "</td>";
+        html += "<td>" + data[i].firstName + "</td>";
+        html += "<td>" + data[i].secondName + "</td>";
+        html += "<td>" + data[i].gender + "</td>";
+        html += "<td>" + data[i].age + "</td>";
+        html += "<td>" + data[i].postcode + "</td>";
+        html += "<td>" + data[i].CHINumber + "</td>";
+        html += "<td>" + data[i].BMI + "</td>";
+        html += "</tr>";
+    }
+
+    document.getElementById("simulatedDataTable").innerHTML = html;
+}
